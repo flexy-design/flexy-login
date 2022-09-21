e6d59b44752442245b30faf06f4737b121af7883
blackmoja <limlim980625@gmail.com>
2022-09-22 02:26:51 +0900

    feat: flexy-login 설정파일 추가

---
+++ bin/flexy-login.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log('👋 - welcome to flexy-login')
+console.log('👋 - flexy-login is a command that set yout authentication for flexy.')
+console.log('👋 - We will support the function soon!')
