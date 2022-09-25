8eb6daf44b40e6a3864bf8216323a1c2ea856814
hmmhmmhm <hmmhmmhm@naver.com>
2022-09-25 19:56:56 +0900

    feat: Update command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import chalk from "chalk";
-import { writeFileSync } from "fs";
-import inquirer from "inquirer";
-import path from "path";
+import chalk from 'chalk'
+import { writeFileSync } from 'fs'
+import inquirer from 'inquirer'
+import path from 'path'
 
-export {};
+export {}
 
 void (async () => {
   console.log(
@@ -12,57 +12,57 @@ void (async () => {
    / /_  / /   / __/  |   /  \\  /
   / __/ / /___/ /___ /   |   / /
  /_/   /_____/_____//_/|_|  /_/`)
-  );
+  )
 
-  console.log(chalk.blueBright(`\nWelcome to Flexy CLI!`));
-  console.log(chalk.blueBright(`Please type figma token to continue.\n`));
+  console.log(chalk.blueBright(`\nWelcome to Flexy CLI!`))
+  console.log(chalk.blueBright(`Please type figma token to continue.\n`))
 
-  console.log(chalk.blueBright(`You can get your token here:`));
+  console.log(chalk.blueBright(`You can get your token here:`))
 
   console.log(
     chalk.bgBlueBright(` https://www.figma.com/developers/api#access-tokens \n`)
-  );
+  )
 
   console.log(
     chalk.blueBright(
       `Click on the link above and click (and copy)
-on the ${chalk.bgBlueBright(" + Get personal access token ")}\n`
+on the ${chalk.bgBlueBright(' + Get personal access token ')}\n`
     )
-  );
+  )
 
   const { personalAccessToken } = await inquirer.prompt([
     {
-      type: "password",
-      name: "personalAccessToken",
-      message: "Figma token:",
-    },
-  ]);
+      type: 'password',
+      name: 'personalAccessToken',
+      message: 'Figma token:'
+    }
+  ])
 
   const { betaTesterToken } = await inquirer.prompt([
     {
-      type: "password",
-      name: "betaTesterToken",
-      message: "Beta Tester Token:",
-    },
-  ]);
+      type: 'password',
+      name: 'betaTesterToken',
+      message: 'Beta Tester Token:'
+    }
+  ])
 
   writeFileSync(
-    path.join(process.cwd(), "flexy.secret.json"),
+    path.join(process.cwd(), 'flexy.secret.json'),
     JSON.stringify(
       {
         personalAccessToken,
-        betaTesterToken,
+        betaTesterToken
       },
       null,
       2
     )
-  );
+  )
 
   console.log(
     chalk.blueBright(
       `\nSuccess! You can now use the ${chalk.bgBlueBright(
-        " npm run init "
+        ' npx flexy-init '
       )} command.\n`
     )
-  );
-})();
+  )
+})()
